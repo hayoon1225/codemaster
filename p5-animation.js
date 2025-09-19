0ce6d7a8bda710be20dd88027f2964af944ccbb7
yoonhu-lab <yoonhu3137@gmail.com>
2025-09-19 16:23:31 +0900

    Update p5-animation.js

--- p5-animation.js
+++ p5-animation.js
@@ -1,6 +1,6 @@
 let angle = [-0.3, 0.1, 0.0]; 
-let omega = [0.01, 0.1, 0.01]; 
-let I = [1, 2, 3]; 
+let omega = [0.01, 0.1, 0.01]; //어느축을 중심으로 회전할지 결정
+let I = [3, 6, 7]; //관성모멘트를 다르게해 불안정한 회전 유도
 
 
 function setup() {
@@ -19,7 +19,6 @@ function draw() {
     ((I[0] - I[1]) / I[2]) * omega[0] * omega[1]  
   ];
 
-
   omega[0] += dOmega[0] * 0.05;
   omega[1] += dOmega[1] * 0.05;
   omega[2] += dOmega[2] * 0.05;
@@ -36,8 +35,10 @@ function draw() {
 
   fill(100, 200, 250);
   stroke(255);
-  box(200, 60, 40); 
+  box(200, 60, 40);
+  fill('green') 
   box(40,60,200);
   translate(0,-60,0);
+  fill('red');
   box(40,120,40);
 }
